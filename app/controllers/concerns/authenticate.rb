81fac7689d4054a5931f270c81851c02b07996a4
shri <shrija.tumma1994@gmail.com>
2020-09-20 18:24:35 +0530

    Review Controller

--- app/controllers/concerns/authenticate.rb
+++ app/controllers/concerns/authenticate.rb
@@ -1,6 +1,6 @@
 module Authenticate
 	def current_user
-		auth_token = request.header["AUTH-TOKEN"]
+		auth_token = request.headers["AUTH-TOKEN"]
 		return unless auth_token
 		@current_user = User.find_by(authentication_token: auth_token)
 	end
@@ -8,5 +8,9 @@ module Authenticate
 	def authenticate_with_token!
 		return if current_user
 		json_response "Unauthenticated", false, {}, :unauthorized
+	end
+
+	def correct_user user
+		user.id == current_user.id		
 	end
 end
