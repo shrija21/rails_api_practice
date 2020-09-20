afb4fcb8f747ce4c6812ced3c6a2f5d053493db3
shri <shrija.tumma1994@gmail.com>
2020-09-20 16:48:15 +0530

    book controller and fake dats

--- Gemfile
+++ Gemfile
@@ -28,6 +28,7 @@ gem 'rack-cors'
 gem 'devise'
 gem 'simple_token_authentication', '~> 1.0'
 gem 'koala', '~> 3.0.0'
+gem 'faker'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- app/controllers/concerns/authenticate.rb
+++ app/controllers/concerns/authenticate.rb
@@ -2,7 +2,7 @@ module Authenticate
 	def current_user
 		auth_token = request.header["AUTH-TOKEN"]
 		return unless auth_token
-		@current_user = User.find_by(authentication: token: auth_token)
+		@current_user = User.find_by(authentication_token: auth_token)
 	end
 
 	def authenticate_with_token!
